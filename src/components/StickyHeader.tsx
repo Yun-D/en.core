f1890578b7ea8a0ab526b6052dcb25bc2bc1af15
Yun-D <yund_272@naver.com>
2026-06-05 00:58:38 +0900

    ✨feat: 스티키 헤더 ref 범위 변경, 곡 추가 버튼 추가
    기존) 히어로섹션 -> 변경) 곡리스트 나오기 전까지

--- src/components/StickyHeader.tsx
+++ src/components/StickyHeader.tsx
@@ -2,28 +2,29 @@ import { useEffect, useState, type RefObject } from "react";
 
 type StickyHeaderProps = {
   title: string;
-  heroRef: RefObject<HTMLDivElement | null>; // HeroSection의 ref 전달받음(기본값은 null도 가능하도록 설정)
+  preListRef: RefObject<HTMLDivElement | null>; // 곡 리스트가 나오기 전의 DOM 요소 ref 전달받음(기본값은 null도 가능하도록 설정)
   children?: React.ReactNode; // 필터칩 등 추가 가능하도록 children 허용
 };
-const StickyHeader = ({ title, heroRef, children }: StickyHeaderProps) => {
+const StickyHeader = ({ title, preListRef, children }: StickyHeaderProps) => {
   const [isVisible, setIsVisible] = useState(false);
 
   useEffect(() => {
-    if (!heroRef.current) return; // HeroSection의 DOM 요소가 없을 때 스티키 헤더 보이도록
+    if (!preListRef.current) return; // 곡 리스트가 나오기 전의 DOM 요소가 없을 때 스티키 헤더 보이도록
 
     const observer = new IntersectionObserver(
-      // HeroSection이 뷰포트에서 사라지는지 감지하는 옵저버. 화면에 보이면 isVisible을 false로, 사라지면 true로 설정
+      // DOM 요소가 뷰포트에서 사라지는지 감지하는 옵저버. 화면에 보이면 isVisible을 false로, 사라지면 true로 설정
       ([entry]) => setIsVisible(!entry.isIntersecting),
     );
 
-    observer.observe(heroRef.current); // 히어로섹션 감시 시작
+    observer.observe(preListRef.current); // DOM 요소 감시 시작
 
     return () => observer.disconnect(); // 컴포넌트 사라질 때 옵저버 정리
-  }, [heroRef]);
+  }, [preListRef]);
 
   return (
     <div
-      className={`fixed inset-x-0 top-0 z-50 mx-auto h-14 w-full max-w-107.5 bg-(--color-surface) px-5 flex items-center justify-between transition-opacity duration-200
+      className={`fixed inset-x-0 top-0 z-50 mx-auto h-14 w-full max-w-107.5 bg-(--color-surface) px-5 flex 
+        items-center justify-between transition-opacity duration-200
     ${isVisible ? "opacity-100" : "opacity-0 pointer-events-none"}`}
     >
       <span className="text-[18px] font-semibold text-(--color-text)">
--- src/pages/MySongs.tsx
+++ src/pages/MySongs.tsx
@@ -10,7 +10,7 @@ import SongCard from "../components/SongCard";
 const MySongs = () => {
   const [isDrawerOpen, setIsDrawerOpen] = useState(false); // 곡 추가 드로어의 열림 상태 관리
 
-  const heroRef = useRef<HTMLDivElement>(null); // HeroSection의 DOM 요소 참조를 위한 ref 생성
+  const preListRef = useRef<HTMLDivElement>(null); // 곡 리스트가 나오기 전의 DOM 요소 참조를 위한 ref 생성
   const tags = useTagStore((state) => state.tags); // 태그 스토어에서 태그 데이터 가져오기
   const songs = useSongStore((state) => state.songs); // 곡 스토어에서 곡 데이터 가져오기
 
@@ -32,71 +32,80 @@ const MySongs = () => {
 
   return (
     <div>
-      <div ref={heroRef}>
+      <div ref={preListRef}>
         <HeroSection
           title="나의 애창곡"
           subtitle={`내 애창곡 ${songs.length} · 나중에 부를 곡 ${songs.filter((song) => song.isLater).length}`}
         />
-      </div>
-      <StickyHeader title="나의 애창곡" heroRef={heroRef} />
 
-      {/* 검색창 + 태그 영역 (곡이 없는 경우 비활성화) ------------------------------------------------ */}
-      <div
-        className={` flex flex-col ${isEmpty ? "opacity-30 pointer-events-none" : ""}`}
-      >
-        {/* 검색창 */}
-        <div
-          className="flex items-center gap-2 border border-(--color-surface-elevated) rounded-xl 
-        bg-(--color-surface) px-4 py-2"
-        >
-          <i className="ti ti-search text-(--color-text-placeholder)" />
-          <input
-            type="text"
-            className="text-sm outline-none w-full"
-            placeholder="곡 제목이나 가수로 검색해보세요"
-          />
-        </div>
-
-        {/* 태그 */}
-        <div className="flex flex-col gap-2 mt-4">
-          <p className="text-xs text-(--color-text-placeholder)">분위기</p>
-          <div className="flex gap-2 flex-wrap">
-            {tags
-              .filter((tag) => tag.category === "mood")
-              .map((tag) => (
-                <button
-                  key={tag.id}
-                  className="cursor-pointer rounded-full border border-(--tag-mood-border) bg-(--tag-mood-bg) text-(--tag-mood-text) 
-                  px-2 py-1 text-xs"
-                >
-                  {tag.label}
-                </button>
-              ))}
-          </div>
-          <p className="text-xs text-(--color-text-placeholder) mt-2">상황</p>
-          <div className="flex gap-2 flex-wrap">
-            {tags
-              .filter((tag) => tag.category === "situation")
-              .map((tag) => (
-                <button
-                  key={tag.id}
-                  className="cursor-pointer rounded-full border border-(--tag-situation-border) bg-(--tag-situation-bg) text-(--tag-situation-text) 
-                  px-2 py-1 text-xs"
-                >
-                  {tag.label}
-                </button>
-              ))}
-          </div>
-        </div>
-
-        <div className="flex justify-between mt-8 items-center">
-          <span className="text-base">나의 애창곡</span>
+        <StickyHeader title="나의 애창곡" preListRef={preListRef}>
           <button
             onClick={onAddClick}
-            className="flex items-center gap-1 text-sm text-(--color-text-placeholder) border border-(--color-surface-elevated) rounded-lg px-3 py-1"
+            className="flex items-center gap-1 text-sm text-(--color-text-primary) 
+            border border-(--color-text-primary) rounded-lg px-3 py-1"
           >
             <i className="ti ti-plus text-xs" />곡 추가
           </button>
+        </StickyHeader>
+
+        {/* 검색창 + 태그 영역 (곡이 없는 경우 비활성화) ------------------------------------------------ */}
+        <div
+          className={` flex flex-col ${isEmpty ? "opacity-30 pointer-events-none" : ""}`}
+        >
+          {/* 검색창 */}
+          <div
+            className="flex items-center gap-2 border border-(--color-surface-elevated) rounded-xl 
+        bg-(--color-surface) px-4 py-2"
+          >
+            <i className="ti ti-search text-(--color-text-placeholder)" />
+            <input
+              type="text"
+              className="text-sm outline-none w-full"
+              placeholder="곡 제목이나 가수로 검색해보세요"
+            />
+          </div>
+
+          {/* 태그 */}
+          <div className="flex flex-col gap-2 mt-4">
+            <p className="text-xs text-(--color-text-placeholder)">분위기</p>
+            <div className="flex gap-2 flex-wrap">
+              {tags
+                .filter((tag) => tag.category === "mood")
+                .map((tag) => (
+                  <button
+                    key={tag.id}
+                    className="cursor-pointer rounded-full border border-(--tag-mood-border) bg-(--tag-mood-bg) text-(--tag-mood-text) 
+                  px-2 py-1 text-xs"
+                  >
+                    {tag.label}
+                  </button>
+                ))}
+            </div>
+            <p className="text-xs text-(--color-text-placeholder) mt-2">상황</p>
+            <div className="flex gap-2 flex-wrap">
+              {tags
+                .filter((tag) => tag.category === "situation")
+                .map((tag) => (
+                  <button
+                    key={tag.id}
+                    className="cursor-pointer rounded-full border border-(--tag-situation-border) bg-(--tag-situation-bg) text-(--tag-situation-text) 
+                  px-2 py-1 text-xs"
+                  >
+                    {tag.label}
+                  </button>
+                ))}
+            </div>
+          </div>
+
+          <div className="flex justify-between mt-8 items-center">
+            <span className="text-base">나의 애창곡</span>
+            <button
+              onClick={onAddClick}
+              className="flex items-center gap-1 text-sm text-(--color-text-placeholder) border border-(--color-surface-elevated) rounded-lg px-3 py-1"
+            >
+              <i className="ti ti-plus text-xs" />곡 추가
+            </button>
+          </div>
         </div>
       </div>
 
